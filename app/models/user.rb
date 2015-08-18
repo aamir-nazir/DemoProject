e46b13e9abe36dd2b710e401609acffad5ea6d40
Aamir Nazir <aamir.nu@gmail.com>
2015-08-18 12:22:44 +0500

    User : Added upload picture feature in edit.html using paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
-  # attr_accessible :title, :body
   has_one :picture, :as => :imageable
   accepts_nested_attributes_for :picture
+
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :picture_attributes
+
+  def profile_picture
+    self.picture.present? ? self.picture : self.build_picture
+  end
+
 end
