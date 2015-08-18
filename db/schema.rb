bf61301b5f987968e2f8e93fee05266ff71937a2
Aamir Nazir <aamir.nu@gmail.com>
2015-08-18 12:53:57 +0500

    Product : Generated scaffold for product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150818050737) do
+ActiveRecord::Schema.define(:version => 20150818073917) do
 
   create_table "pictures", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(:version => 20150818050737) do
   end
 
   add_index "pictures", ["imageable_id"], :name => "index_pictures_on_imageable_id"
+
+  create_table "products", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.decimal  "price",      :precision => 10, :scale => 0
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
