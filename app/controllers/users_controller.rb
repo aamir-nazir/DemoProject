39c46d9f61e2b716d611bfbfbd80dfb58cf3afa6
Aamir Nazir <aamir.nu@gmail.com>
2015-08-20 17:39:05 +0500

    User : Added show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!, only: [:dashboard]
+  before_filter :set_user, only: [:set_user]
+
+  def show
+    @products = @user.products.includes(:pictures).ordered.page(params[:page]).per(User::PRODUCTS_PER_PAGE)
+  end
 
   def dashboard
     @products = current_user.products.includes(:pictures).ordered.page(params[:product_page]).per(User::PRODUCTS_PER_PAGE)
     @reviews = current_user.reviews.includes(:product).page(params[:review_page]).per(Review::PER_PAGE)
   end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 
 end
