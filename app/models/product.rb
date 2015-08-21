9cb0a3c39da87231597869ff0fb55f97a641442b
Aamir Nazir <aamir.nu@gmail.com>
2015-08-21 17:59:09 +0500

    User : Resolved dependency bug of picture as nested attribute

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,11 +7,10 @@ class Product < ActiveRecord::Base
   validates :body, presence: true, length: { maximum: 500 }
   validates :price, presence: true
 
-  accepts_nested_attributes_for :pictures, allow_destroy: true
-
   scope :ordered, -> { order('id DESC') }
 
   has_many :pictures, as: :imageable, dependent: :destroy
+  accepts_nested_attributes_for :pictures, allow_destroy: true
   has_many :reviews, dependent: :destroy
 
   belongs_to :user
