9d0c7d3cf09b3d567791ca41dc532688734f7de1
Aamir Nazir <aamir.nu@gmail.com>
2015-08-21 15:45:46 +0500

    Application Helper : Added helper for validations in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_authorized(id)
+    user_signed_in? && current_user.id == id
+  end
+
+  def select_tab_class(tab_name)
+    params[:controller] == tab_name ? "selected" : ""
+  end
 end
