676032079c81302b01afdca0d9ffcb6ac73d3a79
Aamir Nazir <aamir.nu@gmail.com>
2015-08-24 12:15:30 +0500

    Active_Admin: Added associations of product in admin panel

---
+++ app/admin/pictures.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register Picture do
+ belongs_to :product, parent_class: Product
+end
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,25 @@
 ActiveAdmin.register Product do
+ scope :ordered
+
+ index do
+   column :title
+   column :body
+   column "Created At", :created_at
+   column :price, :sortable => :price do |product|
+     div class: "price" do
+       number_to_currency product.price
+     end
+   end
+
+   column :reviews do |review|
+     link_to "reviews", admin_product_reviews_path(review)
+   end
+
+   column :pictures do |picture|
+     link_to "pictures", admin_product_pictures_path(picture)
+   end
+
+   default_actions
+ end
 
 end
