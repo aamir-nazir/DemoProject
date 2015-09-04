1234f3727368535fd56467e2eae31a3a7b087562
Aamir Nazir <aamir.nu@gmail.com>
2015-09-04 14:29:43 +0500

    Application : Added configuration for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,6 @@ module DemoProject
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.app_generators.scaffold_controller = :scaffold_controller
+    config.assets.initialize_on_precompile = false
   end
 end
